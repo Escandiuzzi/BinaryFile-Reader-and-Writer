abadf0f641dc115487ffc7445962c4bdb28252f0
Luiz Felipe Escandiuzzi <37418595+Escandiuzzi@users.noreply.github.com>
2020-09-20 14:59:07 -0300

    ta indo

--- CSharpProgramming/FileReader.cs
+++ CSharpProgramming/FileReader.cs
@@ -12,19 +12,44 @@ namespace CSharpProgramming
         {
             using (BinaryReader b = new BinaryReader(File.Open(AssetsPath.OUTPUT_PATH, FileMode.Open)))
             {
-                bool searching = true;
-                if (b.BaseStream.Length != 0)
+                int fileLength = (int)b.BaseStream.Length;
+
+                b.Close();
+                
+                return BinarySearch(record, 0, fileLength, 0, false);
+            }
+        }
+
+        private bool BinarySearch(int key, int start, int end, int pos, bool found) 
+        {
+            int middle = 0;
+
+            if (end - start > 0) 
+            {
+                using (BinaryReader b = new BinaryReader(File.Open(AssetsPath.OUTPUT_PATH, FileMode.Open)))
                 {
-                    int middle =  (int)(b.BaseStream.Length / 2) + 1;
-                    while (searching)
-                    {
+                    middle = ((start + end) / 2) + 1;
+
+                    b.BaseStream.Seek(pos, SeekOrigin.Current);
+
+                    var recordFound = b.ReadUInt16();
+                    var position = (int)b.BaseStream.Position;
+                    b.Close();
                     
-                    }
+                    if (recordFound > key)
+                        BinarySearch(key, start, middle, position, false);
+                    else if (recordFound == key)
+                        return true;
+                    else
+                        BinarySearch(key, middle, end, position, false);
+
                 }
             }
 
+
             return false;
-        }
+        }    
+
         private static string DecodeData(byte[] data)
         {
             var encoding = new UTF8Encoding();
