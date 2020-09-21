a83f4f8f5f192527aea181cd30e57aa07552d9d3
leonardomartelli <bwc.leonardo@gmail.com>
2020-09-20 21:08:16 -0300

    a old wise man once said "commit and pray"

--- CSharpProgramming/FileReader.cs
+++ CSharpProgramming/FileReader.cs
@@ -1,7 +1,6 @@
 ﻿using CSharpProgramming.Assets;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,74 +9,86 @@ namespace CSharpProgramming
 {
     public class FileReader
     {
+        private List<(int, int)> _lines;
 
-        private static List<Tuple> _lines;
         public bool Search(string record)
         {
-            _lines = new List<byte[]>();
+            _lines = new List<(int,int)>();
 
-            var rawBuffer = new byte[1024 * 1024];
-            using(BinaryReader b = new BinaryReader(File.Open(AssetsPath.OUTPUT_PATH, FileMode.Open)))
+            var rawBuffer = new byte[1024];
+            using BinaryReader b = new BinaryReader(File.Open(AssetsPath.OUTPUT_PATH, FileMode.Open));
+
+            var bytesBuffered = 0;
+            var bytesConsumed = 0;
+
+            while(true)
             {
-                var bytesBuffered = 0;
-                var bytesConsumed = 0;
+                var bytesRead = b.Read(rawBuffer, bytesBuffered, rawBuffer.Length - bytesBuffered);
 
-                while(true)
+                if(bytesRead == 0) break;
+                bytesBuffered += bytesRead;
+
+                int linePosition;
+
+                do
                 {
-                    var bytesRead = b.Read(rawBuffer, bytesBuffered, rawBuffer.Length - bytesBuffered);
+                    linePosition = Array.IndexOf(rawBuffer, (byte)'\n', bytesConsumed,
+                        bytesBuffered - bytesConsumed);
 
-                    if(bytesRead == 0) break;
-                    bytesBuffered += bytesRead;
-
-                    int linePosition;
-
-                    do
+                    if(linePosition >= 0)
                     {
-                        linePosition = Array.IndexOf(rawBuffer, (byte)'\n', bytesConsumed,
-                            bytesBuffered - bytesConsumed);
+                        var lineLength = linePosition - bytesConsumed;
+                        _lines.Add((linePosition + 1, lineLength));
+                        bytesConsumed += lineLength + 1;
+                    }
 
-                        if(linePosition >= 0)
-                        {
-                            var lineLength = linePosition - bytesConsumed;
-                            var line = new Span<byte>(rawBuffer, bytesConsumed, lineLength);
+                } while(linePosition >= 0);
 
-                            _lines.Add(line.ToArray());
-                            bytesConsumed += lineLength + 1;
-                        }
-
-                    } while(linePosition >= 0);
-
-                    Array.Copy(rawBuffer, bytesConsumed, rawBuffer, 0, (bytesBuffered - bytesConsumed));
-                    bytesBuffered -= bytesConsumed;
-                    bytesConsumed = 0;
-                }
+                Array.Copy(rawBuffer, bytesConsumed, rawBuffer, 0, (bytesBuffered - bytesConsumed));
+                bytesBuffered -= bytesConsumed;
+                bytesConsumed = 0;
             }
+
+            b.Close();
+            b.Dispose();
 
             return SearchOnLine(record, 0, _lines.Count);
         }
 
         private bool SearchOnLine(string key, int start, int end)
         {
+            var rawBuffer = new byte[1024];
+            using BinaryReader b = new BinaryReader(File.Open(AssetsPath.OUTPUT_PATH, FileMode.Open));
+
             if(end - start > 0)
             {
                 var middle = ((end + start) / 2);
 
-                var span = _lines.ElementAt(middle).AsSpan();
+                var line = _lines.ElementAt(middle);
 
-                var commaPos = span.IndexOf((byte)';');
-                var index = span.Slice(0, commaPos - 1);
+                var pos = (int)b.BaseStream.Seek(line.Item1, SeekOrigin.Begin);
 
-                var actual = DecodeData(index.ToArray());
-                var comp = actual.CompareTo(key);
+                b.BaseStream.Read(rawBuffer, pos, line.Item2);
+
+                var actual = DecodeData(new Span<byte>(rawBuffer, pos, Array.IndexOf(rawBuffer, (byte)';', pos) - pos - 1).ToArray());
+                var comp = int.Parse(actual) - int.Parse(key);
 
                 Console.WriteLine($"Chave atual = {actual}, pesquisando = {key}");
 
                 if(comp == 0)
                     return true;
                 else if(comp < 0)
+                {
+                    b.Close();
+                    b.Dispose();
                     return SearchOnLine(key, middle + 1, end);
+                }
                 else if(comp > 0)
-                    return SearchOnLine(key, start, middle);
+                {
+                    b.Close();
+                    b.Dispose();
+                    return SearchOnLine(key, start, middle - 1);
+                }
             }
             return false;
         }
